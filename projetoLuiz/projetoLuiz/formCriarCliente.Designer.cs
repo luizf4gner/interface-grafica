6edbc1c1a3919ea8d1876ad3066473cc076671d0
luizf4gner <luizfagnersilva43@gmail.com>
2024-10-16 21:27:01 -0300

    Add files via upload

---
+++ projetoLuiz/projetoLuiz/formCriarCliente.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace projetoLuiz
+{
+    partial class formCriarCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            textBoxNome = new TextBox();
+            textBoxTelefone = new TextBox();
+            textBoxEmail = new TextBox();
+            textBoxRua = new TextBox();
+            textBoxNumero = new TextBox();
+            textBoxComplemento = new TextBox();
+            textBoxCidade = new TextBox();
+            textBoxEstado = new TextBox();
+            textBoxCep = new TextBox();
+            buttonOk = new Button();
+            buttonCancelar = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 38);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Telefone:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(13, 67);
+            label3.Name = "label3";
+            label3.Size = new Size(44, 15);
+            label3.TabIndex = 2;
+            label3.Text = "E-mail:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(17, 97);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Endereço";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(17, 122);
+            label5.Name = "label5";
+            label5.Size = new Size(103, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Logradouro (Rua):";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(17, 151);
+            label6.Name = "label6";
+            label6.Size = new Size(54, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Número:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(18, 179);
+            label7.Name = "label7";
+            label7.Size = new Size(87, 15);
+            label7.TabIndex = 6;
+            label7.Text = "Complemento:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(18, 208);
+            label8.Name = "label8";
+            label8.Size = new Size(47, 15);
+            label8.TabIndex = 7;
+            label8.Text = "Cidade:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(18, 237);
+            label9.Name = "label9";
+            label9.Size = new Size(70, 15);
+            label9.TabIndex = 8;
+            label9.Text = "UF (Estado):";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(17, 266);
+            label10.Name = "label10";
+            label10.Size = new Size(31, 15);
+            label10.TabIndex = 9;
+            label10.Text = "CEP:";
+            // 
+            // textBoxNome
+            // 
+            textBoxNome.Location = new Point(122, 1);
+            textBoxNome.Name = "textBoxNome";
+            textBoxNome.Size = new Size(193, 23);
+            textBoxNome.TabIndex = 10;
+            // 
+            // textBoxTelefone
+            // 
+            textBoxTelefone.Location = new Point(122, 30);
+            textBoxTelefone.Name = "textBoxTelefone";
+            textBoxTelefone.Size = new Size(193, 23);
+            textBoxTelefone.TabIndex = 11;
+            // 
+            // textBoxEmail
+            // 
+            textBoxEmail.Location = new Point(122, 59);
+            textBoxEmail.Name = "textBoxEmail";
+            textBoxEmail.Size = new Size(193, 23);
+            textBoxEmail.TabIndex = 12;
+            // 
+            // textBoxRua
+            // 
+            textBoxRua.Location = new Point(137, 114);
+            textBoxRua.Name = "textBoxRua";
+            textBoxRua.Size = new Size(193, 23);
+            textBoxRua.TabIndex = 13;
+            // 
+            // textBoxNumero
+            // 
+            textBoxNumero.Location = new Point(137, 143);
+            textBoxNumero.Name = "textBoxNumero";
+            textBoxNumero.Size = new Size(193, 23);
+            textBoxNumero.TabIndex = 14;
+            // 
+            // textBoxComplemento
+            // 
+            textBoxComplemento.Location = new Point(137, 171);
+            textBoxComplemento.Name = "textBoxComplemento";
+            textBoxComplemento.Size = new Size(193, 23);
+            textBoxComplemento.TabIndex = 15;
+            // 
+            // textBoxCidade
+            // 
+            textBoxCidade.Location = new Point(137, 200);
+            textBoxCidade.Name = "textBoxCidade";
+            textBoxCidade.Size = new Size(193, 23);
+            textBoxCidade.TabIndex = 16;
+            // 
+            // textBoxEstado
+            // 
+            textBoxEstado.Location = new Point(137, 229);
+            textBoxEstado.Name = "textBoxEstado";
+            textBoxEstado.Size = new Size(193, 23);
+            textBoxEstado.TabIndex = 17;
+            // 
+            // textBoxCep
+            // 
+            textBoxCep.Location = new Point(137, 258);
+            textBoxCep.Name = "textBoxCep";
+            textBoxCep.Size = new Size(193, 23);
+            textBoxCep.TabIndex = 18;
+            // 
+            // buttonOk
+            // 
+            buttonOk.Location = new Point(137, 287);
+            buttonOk.Name = "buttonOk";
+            buttonOk.Size = new Size(75, 23);
+            buttonOk.TabIndex = 19;
+            buttonOk.Text = "OK";
+            buttonOk.UseVisualStyleBackColor = true;
+            buttonOk.Click += buttonOk_Click;
+            // 
+            // buttonCancelar
+            // 
+            buttonCancelar.Location = new Point(256, 287);
+            buttonCancelar.Name = "buttonCancelar";
+            buttonCancelar.Size = new Size(75, 23);
+            buttonCancelar.TabIndex = 20;
+            buttonCancelar.Text = "Cancelar";
+            buttonCancelar.UseVisualStyleBackColor = true;
+            buttonCancelar.Click += buttonCancelar_Click;
+            // 
+            // formCriarCliente
+            // 
+            AcceptButton = buttonOk;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = buttonCancelar;
+            ClientSize = new Size(343, 320);
+            Controls.Add(buttonCancelar);
+            Controls.Add(buttonOk);
+            Controls.Add(textBoxCep);
+            Controls.Add(textBoxEstado);
+            Controls.Add(textBoxCidade);
+            Controls.Add(textBoxComplemento);
+            Controls.Add(textBoxNumero);
+            Controls.Add(textBoxRua);
+            Controls.Add(textBoxEmail);
+            Controls.Add(textBoxTelefone);
+            Controls.Add(textBoxNome);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "formCriarCliente";
+            Text = "Novo Cliente";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TextBox textBoxNome;
+        private TextBox textBoxTelefone;
+        private TextBox textBoxEmail;
+        private TextBox textBoxRua;
+        private TextBox textBoxNumero;
+        private TextBox textBoxComplemento;
+        private TextBox textBoxCidade;
+        private TextBox textBoxEstado;
+        private TextBox textBoxCep;
+        private Button buttonOk;
+        private Button buttonCancelar;
+    }
+}
